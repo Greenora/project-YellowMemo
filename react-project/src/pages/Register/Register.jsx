5ccbcc827db6dad362b591b4d850c9b9dc764caf
seulzutora <seulzutora@gmail.com>
2025-11-04 02:43:07 -0800

    ♻️ chore: Adjust spacing for readability

--- react-project/src/pages/Register/Register.jsx
+++ react-project/src/pages/Register/Register.jsx
@@ -19,54 +19,57 @@ export default function Register() {
 //화면에 보여지는 부분
 
   return(
-      <div className="min-h-screen bg-[#fcfcf8] px-16 pt-20">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-2 text-gray-900">.Yellowmemo</h1>
-          <p className="text-lg sm:text-lg md:text-xl lg:text-2xl text-gray-700 font-normal">
-            Think, memo, create your own idea board by just One-click
-          </p>
+    <div className="min-h-screen bg-[#fcfcf8] px-16 pt-20">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-2 text-gray-900">.Yellowmemo</h1>
+      <p className="text-lg sm:text-lg md:text-xl lg:text-2xl text-gray-700 font-normal">
+        Think, memo, create your own idea board by just One-click
+      </p>
 
-  <div className="flex flex-col items-center w-full mt-24">  
-    <div className="flex flex-col items-center w-full max-w-[700px]">
-      <AuthInputBox                                     // 아이디 입력창
-        value={loginId}                                 // 입력된 ID 값
-        onChange={(e) => setLoginId(e.target.value)}    // 입력될 때 상태 저장
-        placeholder="ID"                                // 흐리게 'ID' 라고 표시
-        hasError={!!error}                               // 에러 있으면 테두리 빨갛게
-      />
-      <AuthInputBox                                     //비번 입력창
-         value={password}                               //입력된 비번값
-         onChange={(e) => setPassword(e.target.value)}  //입력될 때 상태 저장
-         placeholder="PW"                               //흐리게 "PW" 라고 표시
-         type="password"                                //입력값이 ●●● 로 보이게
-         hasError={!!error}                               //에러가 있으면 테두리 빨갛게
-      />
+      <div className="flex flex-col items-center w-full mt-24">  
+        <div className="flex flex-col items-center w-full max-w-[700px]">
+          <AuthInputBox                                     // 아이디 입력창
+            value={loginId}                                 // 입력된 ID 값
+            onChange={(e) => setLoginId(e.target.value)}    // 입력될 때 상태 저장
+            placeholder="ID"                                // 흐리게 'ID' 라고 표시
+            hasError={!!error}                               // 에러 있으면 테두리 빨갛게
+          />
+          <AuthInputBox                                     //비번 입력창
+            value={password}                               //입력된 비번값
+            onChange={(e) => setPassword(e.target.value)}  //입력될 때 상태 저장
+            placeholder="PW"                               //흐리게 "PW" 라고 표시
+            type="password"                                //입력값이 ●●● 로 보이게
+            hasError={!!error}                               //에러가 있으면 테두리 빨갛게
+          />
 
-      <AuthInputBox                                     //비번 다시 확인하는 입력창 
-        value={confirmPw}                               //입력된 확인 비번 값
-        onChange={(e) => setConfirmPw(e.target.value)}  //입력될 때 마다 값 저장
-        placeholder="Confirm PW"                        //흐리게 보이는 안내 문구
-        type="password"                                 //입력값이 ●●● 로 보이게
-        hasError={!!error}                               //에러가 있으면 테두리 빨갛게
-      />
+          <AuthInputBox                                     //비번 다시 확인하는 입력창 
+            value={confirmPw}                               //입력된 확인 비번 값
+            onChange={(e) => setConfirmPw(e.target.value)}  //입력될 때 마다 값 저장
+            placeholder="Confirm PW"                        //흐리게 보이는 안내 문구
+            type="password"                                 //입력값이 ●●● 로 보이게
+            hasError={!!error}                               //에러가 있으면 테두리 빨갛게
+          />
+        </div>
+
+        {error && (
+          <p className="text-[#ff0000] text-base mb-4 w-[400px] text-center">
+            {error}
+          </p>   //에러가 있을 때만 빨간 글씨로 화면에 보여줌 
+        )}
+          
+        <CustomButton 
+          onClick={handleRegister}
+          className="w-[180px] h-14 bg-white text-black rounded-full shadow-[0_2px_8px_0_rgba(0,0,0,0.08)] font-normal text-lg mt-4 mb-6"
+        >
+          Create account
+        </CustomButton>  {/*회원가입 버튼*/}
+          
+        <span className="text-base text-black">
+          I already have{" "}
+          <a href="/login" className="text-[#3b82f6] no-underline hover:underline">
+            account   {/*이미 계정이 있을때 누르면 로그인 페이지로 이동*/}
+          </a>
+        </span>
+      </div>
     </div>
-
-      {error && (
-        <p className="text-[#ff0000] text-base mb-4 w-[400px] text-center">{error}</p>   //에러가 있을 때만 빨간 글씨로 화면에 보여줌 
-      )}
-      
-      <CustomButton 
-      onClick={handleRegister}
-      className="w-[180px] h-14 bg-white text-black rounded-full shadow-[0_2px_8px_0_rgba(0,0,0,0.08)] font-normal text-lg mt-4 mb-6"
-      >Create account
-      </CustomButton>  {/*회원가입 버튼*/}
-      
-      <span className="text-base text-black">
-       I already have{" "}
-      <a href="/login" className="text-[#3b82f6] no-underline hover:underline">
-        account   {/*이미 계정이 있을때 누르면 로그인 페이지로 이동*/}
-      </a>
-      </span>
-    </div>
-  </div>
   )
 }
