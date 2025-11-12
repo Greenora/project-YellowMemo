0fc0dda26a866ce0acce6657683b93ab851a2a17
wowai717 <bjacau3@gmail.com>
2025-11-12 17:28:00 +0900

    🚀 feat: Swagger(api) 설정 및 CORS, ValidationPipe 추가

--- server/src/posts/dto/create-post.dto.ts
+++ server/src/posts/dto/create-post.dto.ts
@@ -1,1 +1,34 @@
-export class CreatePostDto {}
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+// Contents 배열 안의 개별 스티커 객체 타입
+class ContentItemDto {
+  @IsString()
+  @IsNotEmpty()
+  type: 'text' | 'image'; // text 또는 image만 허용
+
+  @IsString()
+  value?: string; // (선택적) type='text'일 경우
+
+  @IsString()
+  url?: string; // (선택적) type='image'일 경우
+
+  @IsNotEmpty()
+  x: number;
+
+  @IsNotEmpty()
+  y: number;
+}
+
+// 최종 게시물 생성 DTO
+export class CreatePostDto {
+  @IsString()   
+  @IsNotEmpty()
+  title: string;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ValidateNested({ each: true }) //  배열 안의 객체도 검사
+  @Type(() => ContentItemDto) // 배열 안의 객체는 ContentItemDto 틀을 사용
+  contents: ContentItemDto[];
+}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
 import type { User } from './entities/user.entity';
 import type { Request } from 'express';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('users') // API /users
 export class UsersController {
@@ -10,6 +11,7 @@ export class UsersController {
 
   // 내 정보 조회 API (GET /users/me)
   @Get('me') // 최종 주소는 GET /users/me
+  @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))   //findme를 실행하기전에 반드시 통과해야함
   findMe(@Req() req: Request): Promise<Partial<User>> {
     const user = req.user as User;    //Request 객체에서 토큰 검사기가 넣어준 user 정보를 꺼냄
