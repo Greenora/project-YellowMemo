fdb3027a60de67e0f35f864450a217613e60dde4
seulzutora <seulzutora@gmail.com>
2025-11-30 21:46:49 -0800

    ✨ Hook useTextboxes에 API 연결
    useTextboxes hook에 API연결 및 수정

--- react-project/src/hooks/useTextboxes.js
+++ react-project/src/hooks/useTextboxes.js
@@ -1,14 +1,36 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import useCustomFetch from "./useCustomFetch";
 
 export default function useTextboxes(id) {
   const [textboxes, setTextboxes] = useState([]);
+  const customFetch = useCustomFetch();
 
   useEffect(() => {
+    if (!id) return;
+
     // text box 데이터 가져오기
-    fetch(`http://localhost:5000/textbox?postId=${id}`)
-      .then(res => res.json())
-      .then(setTextboxes);
-  }, [id]);
+    const fetchTextboxes = async () => {
+      try {
+        const response = await customFetch(`/posts/${id}`, { method: "GET" });
+        const postData = response.data;
+
+        const contents = postData.contents || [];
+
+        const textData = contents
+          .filter(item => item.type === 'text')
+          .map(item => ({
+            ...item,
+            content: item.value,
+          }));
+
+        setTextboxes(textData);
+      } catch (error) {
+        console.error("textboxes load error:", error);
+        setTextboxes([]);
+      }
+    };
+    fetchTextboxes();
+  }, [id, customFetch]);
 
   return [textboxes, setTextboxes];
 }
